980cdaac90f83d92af892681e4d7d883573d70de
Anmol <anmoljain834@gmail.com>
2024-03-22 17:37:14 +0530

    DataEng page started

--- src/pages/Services/DataEng.jsx
+++ src/pages/Services/DataEng.jsx
@@ -1,7 +1,104 @@
 import React from 'react'
+import "../../Styles/Dataeng.css"
 
 export default function DataEng() {
   return (
-    <div>DataEng</div>
+    <div>
+      <div id='data-part-1'>
+        <img src="https://www.unitrestech.com/assets/images/banner123.png" alt="" />
+        <h1>Data Engineering & Analytics</h1>
+        <p>Accelerate Decision-Making with Reliable Insights</p>
+      </div>
+      <div id='data-part-2'>
+        <h1>Data and Intelligence Solutions that Leverage the Power of Your Data</h1>
+        <div>
+          <div>
+            <img src="https://www.unitrestech.com/assets/images/51.png" alt="" />
+            <h3>Artificial Intelligence</h3>
+            <p>Predict the future to accelerate your business and get a leg up on your competition.</p>
+          </div>
+          <div>
+            <img src="https://www.unitrestech.com/assets/images/52.png" alt="" />
+            <h3>Intelligent Insights</h3>
+            <p>Answer questions at scale with the right tools and skills to leverage your discoverable,
+               available, and trustable data</p>
+          </div>
+          <div>
+            <img src="https://www.unitrestech.com/assets/images/53.svg" alt="" />
+            <h3>Platform Modernization</h3>
+            <p>Utilize and accelerate your data by modernizing your platforms and making your systems, 
+              processes, and results reliable.</p>
+          </div>
+          <div>
+            <img src="https://www.unitrestech.com/assets/images/54.png" alt="" />
+            <h3>Data Fabric</h3>
+            <p>Ensure your data is findable, high quality, and secure so you can confidently take action on it.</p>
+          </div>
+          <div>
+            <img src="https://www.unitrestech.com/assets/images/55.png" alt="" />
+            <h3>Strategy-and-Enablement</h3>
+            <p>Become a data-driven by maximizing the impact of ata at your fingertips.</p>
+          </div>
+        </div>
+      </div>
+      <div id='data-part-2'>
+        <h1>Accelerate Your Business with Data</h1>
+        <div>
+          <div>
+            <img src="https://www.unitrestech.com/assets/images/56.svg" alt="" />
+            <h3>Customer Attribution</h3>
+            <p>Attribute success ( or failure) to specific marketing channels and customer touchpoints.</p>
+          </div>
+          <div>
+            <img src="https://www.unitrestech.com/assets/images/57.svg" alt="" />
+            <h3>Corporate Performance management</h3>
+            <p>We can provide testing support for your mobile app or website. We'll help you...</p>
+          </div>
+          <div>
+            <img src="https://www.unitrestech.com/assets/images/58.svg" alt="" />
+            <h3>Supply Chain</h3>
+            <p>
+            modernize your supply chain to streamline operations, 
+            capture real cost reductions, and build relationships with 
+            customers and suppliers.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h1>Unitres Approach</h1>
+        <p>Integrate your enterprise's operations, workforce, 
+          and valuable insights through the power of intelligent 
+          process automation (IPA) to achieve remarkable efficiency 
+          improvements while keeping costs in check.</p>
+        <img src="https://www.unitrestech.com/assets/images/222.png" alt="" />
+      </div>
+      <div>
+        <h1>Key Partnerships</h1>
+        <div>
+          <img src="https://www.unitrestech.com/assets/images/frame14.png" alt="" />
+          <img src="https://www.unitrestech.com/assets/images/frame13.png" alt="" />
+          <img src="https://www.unitrestech.com/assets/images/frame12.png" alt="" />
+          <img src="https://www.unitrestech.com/assets/images/frame11.png" alt="" />
+        </div>
+      </div>
+      <div>
+        <h1>Why Unitres</h1>
+        <div>
+          <div>
+            <h3>End to end Data Engineering Services</h3>
+            <img src="https://www.unitrestech.com/assets/images/s1.png" alt="" />
+          </div>
+          <div>
+            <h3>Unmatched Expertise</h3>
+            <img src="https://www.unitrestech.com/assets/images/s2.png" alt="" />
+          </div>
+          <div>
+            <h3>Proven Accelerators for Faster time to value</h3>
+            <img src="https://www.unitrestech.com/assets/images/s3.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
